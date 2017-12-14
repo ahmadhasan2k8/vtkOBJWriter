bd325b062e774dba6099a207fc647a34d19b1b85
ahmad-pc <ahmad.hasan2k8@gmail.com>
2017-12-14 15:31:21 -0800

    Changes made to separate out the semi-colon removal from clang-format changes

--- vtkOBJWriter.cxx
+++ vtkOBJWriter.cxx
@@ -35,8 +35,6 @@
 #include "vtkDataSet.h"
 #include "vtkFloatArray.h"
 #include "vtkGeometryFilter.h"
-#include "vtkInformation.h"
-#include "vtkInformationVector.h"
 #include "vtkMapper.h"
 #include "vtkObjectFactory.h"
 #include "vtkPointData.h"
@@ -44,12 +42,14 @@
 #include "vtkProperty.h"
 #include "vtkRenderWindow.h"
 #include "vtkRendererCollection.h"
-#include "vtkSmartPointer.h"
 #include "vtkTransform.h"
+#include "vtkSmartPointer.h"
+#include "vtkInformationVector.h"
+#include "vtkInformation.h"
 
 vtkStandardNewMacro(vtkOBJWriter)
 
-    vtkOBJWriter::vtkOBJWriter()
+vtkOBJWriter::vtkOBJWriter()
 {
   this->FileName = NULL;
 
@@ -59,27 +59,27 @@ vtkStandardNewMacro(vtkOBJWriter)
 
 vtkOBJWriter::~vtkOBJWriter()
 {
-  if (this->FileName)
-  {
-    delete[] this->FileName;
-  }
+  if ( this->FileName )
+    {
+    delete [] this->FileName;
+    }
 }
 
 int vtkOBJWriter::RequestData(vtkInformation *vtkNotUsed(request),
-                              vtkInformationVector **inputVector,
-                              vtkInformationVector *vtkNotUsed(outputVector))
+                               vtkInformationVector **inputVector,
+                               vtkInformationVector *vtkNotUsed(outputVector))
 {
-  // Get the input
-  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
-  vtkPolyData *pd =
-      vtkPolyData::SafeDownCast(inInfo->Get(vtkDataObject::DATA_OBJECT()));
 
-  // open the file for writing
+  //Get the input
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkPolyData *pd = vtkPolyData::SafeDownCast(
+    inInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+  //open the file for writing
   std::ofstream fout(this->FileName);
 
-  // write header
-  fout << "# wavefront obj file written by the visualization toolkit" << endl
-       << endl;
+  //write header
+  fout << "# wavefront obj file written by the visualization toolkit" << endl << endl;
 
   fout << "mtllib NONE" << endl << endl;
 
@@ -90,39 +90,39 @@ int vtkOBJWriter::RequestData(vtkInformation *vtkNotUsed(request),
   int idStart = 1;
   double p[3];
   vtkCellArray *cells;
-  vtkIdType npts  = 0;
+  vtkIdType npts = 0;
   vtkIdType *indx = 0;
 
   // write out the points
   for (i = 0; i < pd->GetNumberOfPoints(); i++)
-  {
+    {
     pd->GetPoint(i, p);
     fout << "v " << p[0] << " " << p[1] << " " << p[2] << endl;
-  }
+    }
 
   idNext = idStart + static_cast<int>(pd->GetNumberOfPoints());
 
   // write out the point data
 
   vtkSmartPointer<vtkDataArray> normals = pd->GetPointData()->GetNormals();
-  if (normals)
-  {
-    for (i = 0; i < normals->GetNumberOfTuples(); i++)
+  if(normals)
     {
-      normals->GetTuple(i, p);
-      fout << "vn " << p[0] << " " << p[1] << " " << p[2] << endl;
+      for (i = 0; i < normals->GetNumberOfTuples(); i++)
+        {
+        normals->GetTuple(i, p);
+        fout << "vn " << p[0] << " " << p[1] << " " << p[2] << endl;
+        }
     }
-  }
 
   tcoords = pd->GetPointData()->GetTCoords();
   if (tcoords)
-  {
-    for (i = 0; i < tcoords->GetNumberOfTuples(); i++)
     {
+    for (i = 0; i < tcoords->GetNumberOfTuples(); i++)
+      {
       tcoords->GetTuple(i, p);
       fout << "vt " << p[0] << " " << p[1] << endl;
+      }
     }
-  }
 
   // write out a group name and material
   fout << endl << "g grp" << idStart << endl;
@@ -130,164 +130,145 @@ int vtkOBJWriter::RequestData(vtkInformation *vtkNotUsed(request),
 
   // write out verts if any
   if (pd->GetNumberOfVerts() > 0)
-  {
-    cells = pd->GetVerts();
-    for (cells->InitTraversal(); cells->GetNextCell(npts, indx);)
     {
-      fout << "p ";
-      for (i = 0; i < npts; i++)
+    cells = pd->GetVerts();
+    for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
       {
-        fout << static_cast<int>(indx[i]) + idStart << " ";
+        fout << "p ";
+        for (i = 0; i < npts; i++)
+          {
+          fout << static_cast<int>(indx[i])+idStart << " ";
+          }
+        fout << endl;
       }
-      fout << endl;
     }
-  }
 
   // write out lines if any
   if (pd->GetNumberOfLines() > 0)
-  {
-    cells = pd->GetLines();
-    for (cells->InitTraversal(); cells->GetNextCell(npts, indx);)
     {
-      fout << "l ";
-      if (tcoords)
+    cells = pd->GetLines();
+    for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
       {
-        for (i = 0; i < npts; i++)
-        {
-          fout << static_cast<int>(indx[i]) + idStart << "/"
-               << static_cast<int>(indx[i]) + idStart << " ";
-        }
+        fout << "l ";
+        if (tcoords)
+          {
+          for (i = 0; i < npts; i++)
+            {
+            fout << static_cast<int>(indx[i])+idStart << "/" << static_cast<int>(indx[i]) + idStart << " ";
+            }
+          }
+        else
+          {
+          for (i = 0; i < npts; i++)
+            {
+            fout << static_cast<int>(indx[i])+idStart << " ";
+            }
+          }
+        fout << endl;
       }
-      else
-      {
-        for (i = 0; i < npts; i++)
-        {
-          fout << static_cast<int>(indx[i]) + idStart << " ";
-        }
-      }
-      fout << endl;
     }
-  }
 
   // write out polys if any
   if (pd->GetNumberOfPolys() > 0)
-  {
-    cells = pd->GetPolys();
-    for (cells->InitTraversal(); cells->GetNextCell(npts, indx);)
     {
+    cells = pd->GetPolys();
+    for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
+      {
       fout << "f ";
       for (i = 0; i < npts; i++)
-      {
+        {
         if (normals)
-        {
+          {
           if (tcoords)
-          {
-            fout << static_cast<int>(indx[i]) + idStart << "/"
-                 << static_cast<int>(indx[i]) + idStart << "/"
-                 << static_cast<int>(indx[i]) + idStart << " ";
-          }
+            {
+            fout << static_cast<int>(indx[i])+idStart << "/" << static_cast<int>(indx[i])+ idStart << "/" << static_cast<int>(indx[i]) + idStart << " ";
+            }
           else
-          {
-            fout << static_cast<int>(indx[i]) + idStart << "//"
-                 << static_cast<int>(indx[i]) + idStart << " ";
+            {
+            fout << static_cast<int>(indx[i])+idStart << "//" << static_cast<int>(indx[i]) + idStart << " ";
+            }
           }
-        }
         else
-        {
+          {
           if (tcoords)
-          {
-            fout << static_cast<int>(indx[i]) + idStart << "/"
-                 << static_cast<int>(indx[i]) + idStart << " ";
-          }
+            {
+            fout << static_cast<int>(indx[i])+idStart << "/" << static_cast<int>(indx[i]) + idStart << " ";
+            }
           else
-          {
-            fout << static_cast<int>(indx[i]) + idStart << " ";
+            {
+            fout << static_cast<int>(indx[i])+idStart << " ";
+            }
           }
         }
+        fout << endl;
       }
-      fout << endl;
     }
-  }
 
   // write out tstrips if any
   if (pd->GetNumberOfStrips() > 0)
-  {
-    cells = pd->GetStrips();
-    for (cells->InitTraversal(); cells->GetNextCell(npts, indx);)
     {
-      for (i = 2; i < npts; i++)
+    cells = pd->GetStrips();
+    for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
       {
-        if (i % 2)
+      for (i = 2; i < npts; i++)
         {
-          i1 = i - 1;
-          i2 = i - 2;
+        if (i%2)
+        {
+            i1 = i - 1;
+            i2 = i - 2;
         }
         else
         {
-          i1 = i - 1;
-          i2 = i - 2;
+            i1 = i - 1;
+            i2 = i - 2;
         }
-        if (normals)
-        {
+        if(normals)
+          {
           if (tcoords)
-          {
-            fout << "f " << static_cast<int>(indx[i1]) + idStart << "/"
-                 << static_cast<int>(indx[i1]) + idStart << "/"
-                 << static_cast<int>(indx[i1]) + idStart << " ";
-            fout << static_cast<int>(indx[i2]) + idStart << "/"
-                 << static_cast<int>(indx[i2]) + idStart << "/"
-                 << static_cast<int>(indx[i2]) + idStart << " ";
-            fout << static_cast<int>(indx[i]) + idStart << "/"
-                 << static_cast<int>(indx[i]) + idStart << "/"
-                 << static_cast<int>(indx[i]) + idStart << endl;
-          }
+            {
+            fout << "f " << static_cast<int>(indx[i1]) + idStart << "/" << static_cast<int>(indx[i1]) + idStart << "/" <<  static_cast<int>(indx[i1]) + idStart << " ";
+            fout << static_cast<int>(indx[i2])+ idStart << "/" << static_cast<int>(indx[i2]) + idStart << "/" << static_cast<int>(indx[i2]) + idStart << " ";
+             fout << static_cast<int>(indx[i]) + idStart << "/" << static_cast<int>(indx[i]) + idStart << "/" << static_cast<int>(indx[i]) + idStart << endl;
+            }
           else
-          {
-            fout << "f " << static_cast<int>(indx[i1]) + idStart << "//"
-                 << static_cast<int>(indx[i1]) + idStart << " ";
-            fout << static_cast<int>(indx[i2]) + idStart << "//"
-                 << static_cast<int>(indx[i2]) + idStart << " ";
-            fout << static_cast<int>(indx[i]) + idStart << "//"
-                 << static_cast<int>(indx[i]) + idStart << endl;
+            {
+            fout << "f " << static_cast<int>(indx[i1]) + idStart << "//" << static_cast<int>(indx[i1]) + idStart << " ";
+            fout << static_cast<int>(indx[i2]) + idStart << "//" << static_cast<int>(indx[i2]) + idStart << " ";
+            fout << static_cast<int>(indx[i]) + idStart << "//" << static_cast<int>(indx[i]) + idStart << endl;
+            }
           }
-        }
         else
-        {
+          {
           if (tcoords)
-          {
-            fout << "f " << static_cast<int>(indx[i1]) + idStart << "/"
-                 << static_cast<int>(indx[i1]) + idStart << " ";
-            fout << static_cast<int>(indx[i2]) + idStart << "/"
-                 << static_cast<int>(indx[i2]) + idStart << " ";
-            fout << static_cast<int>(indx[i]) + idStart << "/"
-                 << static_cast<int>(indx[i]) + idStart << endl;
-          }
+            {
+            fout << "f " << static_cast<int>(indx[i1]) + idStart << "/" << static_cast<int>(indx[i1]) + idStart << " ";
+            fout << static_cast<int>(indx[i2]) + idStart << "/" << static_cast<int>(indx[i2]) + idStart << " ";
+            fout << static_cast<int>(indx[i]) + idStart << "/" << static_cast<int>(indx[i]) + idStart << endl;
+            }
           else
-          {
-            fout << "f " << static_cast<int>(indx[i1]) + idStart << " "
-                 << static_cast<int>(indx[i2]) + idStart << " "
-                 << static_cast<int>(indx[i]) + idStart << endl;
+            {
+            fout << "f " << static_cast<int>(indx[i1]) + idStart << " " << static_cast<int>(indx[i2]) + idStart << " " << static_cast<int>(indx[i]) + idStart << endl;
+            }
           }
         }
       }
     }
-  }
 
   idStart = idNext;
 
   return 1;
 }
 
-void vtkOBJWriter::PrintSelf(ostream &os, vtkIndent indent)
+void vtkOBJWriter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
 
   if (this->FileName)
-  {
+    {
     os << indent << "FileName: " << this->FileName << "\n";
-  }
+    }
   else
-  {
+    {
     os << indent << "FileName: (null)\n";
-  }
+    }
 }
